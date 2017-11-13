6658b43b94b86f99a69f45be80c93cabdf49add3
Jagadeeswar Chirasani <jchirasani@sapient.com>
2017-11-13 07:37:31 +0000

    remote_Execute.groovy edited online with Bitbucket

--- vars/remote_Execute.groovy
+++ vars/remote_Execute.groovy
@@ -17,6 +17,7 @@ def call(body) {
   try {
       def remoteExe = new remote()
       remoteExe.setValue("${config.REMOTE_USER}", "${config.REMOTE_IP}", "${config.DEPLOY_PATH}", "${config.SCRIPT}","${config.SCRIPT_FILE_NAME}")
+      remoteExe.copyScriptFile()
       remoteExe.checkFile()
 	  remoteExe.runCommand()
   }
